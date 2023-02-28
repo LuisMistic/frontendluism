1ebfbe96d6d7ec8a83c261c1cbb9d7e8cb4b3700
Luis Martinez <luis.escalada@hotmail.com>
2023-02-28 20:28:55 -0300

    centrado

--- function.js
+++ function.js
@@ -3,5 +3,5 @@
 document.getElementById('boton').onclick = function() {
 
      console.log("Capturamos el evento clip");
-    document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrlloando contenido audiovisual en la compañina de Teatro MLM (Mi lugar en el Mundo) ";
+    document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrlloando contenido audiovisual en la compania de Teatro, MLM (Mi lugar en el Mundo) ";
 }
