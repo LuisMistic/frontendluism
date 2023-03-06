96b605ea031fc56ba446a9e25dfc0568c79adf27
Luis Martinez <luis.escalada@hotmail.com>
2023-03-05 23:06:42 -0300

    agregando botonera con link de alnclaje

--- function.js
+++ function.js
@@ -2,6 +2,6 @@
 
 document.getElementById('boton').onclick = function() {
 
-     console.log("Capturamos el evento clip");
+    
     document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrlloando contenido audiovisual en la compania de Teatro, MLM (Mi lugar en el Mundo) ";
 }
