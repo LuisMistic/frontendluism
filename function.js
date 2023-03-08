50a4f4401add8b89e3943e8707b137adadd38947
Luis Martinez <luis.escalada@hotmail.com>
2023-03-08 19:47:18 -0300

    diferenes estilos

--- function.js
+++ function.js
@@ -6,7 +6,12 @@ document.getElementById('boton').onclick = function() {
     document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrllando contenido audiovisual en la compania de Teatro, MLM (Mi lugar en el Mundo) ";
 }
 
+document.getElementById('boton_color0').addEventListener('click', function () {
 
+    document.body.style.backgroundColor = '#ffffff';
+    console.log("aa")
+
+});
 
 document.getElementById('boton_color').addEventListener('click', function () {
 
@@ -15,6 +20,27 @@ document.getElementById('boton_color').addEventListener('click', function () {
 
 });
 
+
+document.getElementById('boton_color2').addEventListener('click', function () {
+
+    document.body.style.backgroundColor = '#FF8C00';
+    console.log("aa")
+
+});
+
+document.getElementById('boton_color1').addEventListener('click', function () {
+
+    document.body.style.backgroundColor = '#000000';
+    console.log("aa")
+
+});
+
+document.getElementById('boton_color3').addEventListener('click', function () {
+
+    document.body.style.backgroundColor = '#2F4F4F';
+    console.log("aa")
+
+});
 
 
 
