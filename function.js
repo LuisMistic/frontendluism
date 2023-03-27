1074ec109bdf21825ced6074735e07a04f8b5985
Luis Martinez <luis.escalada@hotmail.com>
2023-03-26 23:34:25 -0300

    modificacion estructura general

--- function.js
+++ function.js
@@ -1,10 +1,17 @@
 
 
-document.getElementById('boton').onclick = function() {
+document.getElementById('boton1').onclick = function() {
 
     
     document.getElementById("sobremi").innerHTML = " Me considero una persona flexible, con alta capacidad de  adaptabilidad y aprendizaje. Actualmente me encuentro trabajando en KOMO en la digitalizacion y el ingreso de datos como tambien desarrllando contenido audiovisual en la compania de Teatro, MLM (Mi lugar en el Mundo) ";
 }
+
+document.getElementById('boton2').onclick = function () {
+
+
+    document.getElementById("sobremi").innerHTML = "Los programas con los que trabajo son los siguientes:  DISEÑO GRAFICO – EDICION DE VIDEO- EDICION DE SONIDO  (Paquete de  Adobe Cloud)  – ANIMACION 3D (Cinema 4d)  DESARROLLADOR WEB (Visual Studio Code – Apache NetBean)   ";
+}
+
 
 document.getElementById('boton_color0').addEventListener('click', function () {
 
